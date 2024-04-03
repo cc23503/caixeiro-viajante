d9f913dc48e7e1616db6e64580222bf106271d24
cc23503 <cc23503@g.unicamp.br>
2024-04-03 19:50:06 -0300

    Comentando métodos pré criação.

---
+++ main.py
@@ -0,0 +1,9 @@
+# Método para passar pelas listas dentro da matriz
+
+# Método para encontrar indices impares (listas de distância)
+
+# Método para passar pelas distâncias dentro das 
+# respectivas cidade.
+
+# Método para identificar distâncias impares dentro das
+# cidades.
